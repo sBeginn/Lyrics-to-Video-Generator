0fb9c1d5b58c0faa105a7189815c51ff762b4176
Steffen Beginn <115171161+sBeginn@users.noreply.github.com>
2023-11-13 12:11:02 +0100

    Update diffusers_test_py.py

--- diffusers_test_py.py
+++ diffusers_test_py.py
@@ -3,10 +3,12 @@ import torch
 from matplotlib import pyplot as plt
 
 pipeline = AutoPipelineForText2Image.from_pretrained(
-    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, safety_checker = None, requires_safety_checker = False
+    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, safety_checker=None, requires_safety_checker=False
 ).to("cuda")
 
 prompt = "green car"
 
-image = pipeline(prompt, num_inference_steps=2).images[0]
-plt.imshow(image)
+generated_image = pipeline(prompt, num_inference_steps=2).images[0]
+
+plt.imshow(generated_image)
+plt.show()
